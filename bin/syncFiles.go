ce1ab57465b6b1137c6eb0f64cd0f7e7a99f5bd4
Daenges <daenges2@gmail.com>
2021-03-14 21:11:32 +0100

    Move config check in own func.

--- bin/syncFiles.go
+++ bin/syncFiles.go
@@ -17,40 +17,18 @@ func main() {
 	fmt.Println("Starting sync process.")
 	for {
 		startProc()
-		time.Sleep(time.Millisecond*time.Duration(getArgs()))
+		time.Sleep(time.Millisecond * time.Duration(getArgs()))
 	}
 }
 
 func startProc() {
-	var dat []byte
-	if wd, err := os.Getwd(); isFile(wd + getPathSeperator() + "config.txt") && err == nil {
-		dat, err = ioutil.ReadFile(wd + getPathSeperator() + "config.txt")
-		check(err)
-	} else if len(os.Args[1:]) > 0 {
-		dat, err = ioutil.ReadFile(os.Args[1])
-		check(err)
-	} else {
 
-		if path, err := os.Getwd(); err == nil {
-			emptyFile, err := os.Create(path + "\\config.txt")
-			check(err)
-			defer emptyFile.Close()
-
-			dat = []byte("# Enter configuration (one File per line) as described in the help page.")
-			emptyFile.Write(dat)
-
-			log.Printf("Could not find config. Created one at %v\\config.txt", path)
-		} else {
-			log.Fatal("Could not get working directory. Please create a config file, so the program can operate properly.")
-		}
-	}
-
-	fileData := strings.Split(string(dat), "\n")
+	fileData := strings.Split(string(getConfigOrCreateIt()), "\n")
 
 	// Entry format: PATHFROM -> PathTo1 PathTo2 PathTo3
 	if !isAllComented(fileData) && len(fileData) > 0 {
 		for _, line := range fileData {
-			if len(line) > 0 && line[0] != '#' {	// Allow comments
+			if len(line) > 0 && line[0] != '#' { // Allow comments
 				go detectAndExecuteOperation(line) // todo make go later
 			}
 		}
@@ -58,6 +36,31 @@ func startProc() {
 	} else {
 		log.Fatalf("The file was empty!")
 	}
+}
+
+func getConfigOrCreateIt() (fileContent []byte) {
+	if wd, err := os.Getwd(); isFile(wd+getPathSeperator()+"config.txt") && err == nil {
+		fileContent, err = ioutil.ReadFile(wd + getPathSeperator() + "config.txt")
+		check(err)
+	} else if len(os.Args[1:]) > 0 {
+		fileContent, err = ioutil.ReadFile(os.Args[1])
+		check(err)
+	} else {
+
+		if path, err := os.Getwd(); err == nil {
+			emptyFile, err := os.Create(path + getPathSeperator() + "config.txt")
+			check(err)
+			defer emptyFile.Close()
+
+			fileContent = []byte("# Enter configuration (one File per line) as described in the help page.")
+			emptyFile.Write(fileContent)
+
+			log.Printf("Could not find config. Created one at %v%vconfig.txt", path, getPathSeperator())
+		} else {
+			log.Fatal("Could not get working directory. Please create a config file, so the program can operate properly.")
+		}
+	}
+	return
 }
 
 func detectAndExecuteOperation(line string) {
